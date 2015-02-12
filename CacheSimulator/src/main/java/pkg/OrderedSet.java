9045f1833a914d86682b24a4e57c284e15428f61
Kenny Ruddick <ruddick1@tcnj.edu>
2015-02-12 17:11:56 -0500

    Added some documentation for the OrderedSet

--- CacheSimulator/src/main/java/pkg/OrderedSet.java
+++ CacheSimulator/src/main/java/pkg/OrderedSet.java
@@ -1,6 +1,15 @@
 package pkg;
 
 public interface OrderedSet {
+	/**
+	 * Adds a tag to the set if it is not already in the set. Returns TRUE for a
+	 * hit and FALSE for a miss.
+	 * 
+	 * @param tag
+	 *            The tag to be added to set.
+	 * @return Whether or not the set already contains this tag.
+	 */
 	boolean add(int tag);
+
 	Integer[] toArray();
 }
