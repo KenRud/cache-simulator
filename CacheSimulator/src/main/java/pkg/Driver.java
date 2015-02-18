28a78cc487d68e84c84c3801b986d9b48b52d88b
Joseph Urciuoli <urciuoj1@tcnj.edu>
2015-02-18 10:54:45 -0500

    Added Second Trace

--- CacheSimulator/src/main/java/pkg/Driver.java
+++ CacheSimulator/src/main/java/pkg/Driver.java
@@ -15,14 +15,14 @@ public class Driver {
 
 		PrintWriter writer = new PrintWriter("output.txt", "UTF-8");
 		writer.println("Simulation Results\n");
+		writer.println("Using " + firstTrace);
 		writer.println("Simulation:\tMisses:\tAddresses:\tMiss Rate:\n");
-
 		while(K <= 4){
 
 			Simulation sim = new Simulation(KN, (int) Math.pow(2,  K), Simulation.FIFO);
 			sim.run(firstTrace);
 			writer.println(Simulation.FIFO + "\tKN: " + KN + "\tK: " + Math.pow(2,  K) + "\t");
-			writer.println(sim.getMisses() + "\t" + sim.getAdresses() + "\t" + (100f * sim.getMisses() / sim.getAdresses()) + "\n");
+			writer.print(sim.getMisses() + "\t" + sim.getAdresses() + "\t" + (100f * sim.getMisses() / sim.getAdresses()) + "\n");
 
 			K++;
 			
@@ -40,7 +40,44 @@ public class Driver {
 			Simulation sim = new Simulation(KN, (int) Math.pow(2,  K), Simulation.LRU);
 			sim.run(firstTrace);
 			writer.println(Simulation.LRU + "\tKN: " + KN + "\tK: " + Math.pow(2,  K) + "\t");
-			writer.println(sim.getMisses() + "\t" + sim.getAdresses() + "\t" + (100f * sim.getMisses() / sim.getAdresses()) + "\n");
+			writer.print(sim.getMisses() + "\t" + sim.getAdresses() + "\t" + (100f * sim.getMisses() / sim.getAdresses()) + "\n");
+
+			K++;
+			
+			if(KN == 64 && K == 5){
+				KN = 256;
+				K = 1;
+			}
+		}
+
+		K = 1;
+		KN = 64;
+		writer.println("Using " + secondTrace);
+
+		while(K <= 4){
+
+			Simulation sim = new Simulation(KN, (int) Math.pow(2,  K), Simulation.FIFO);
+			sim.run(secondTrace);
+			writer.println(Simulation.FIFO + "\tKN: " + KN + "\tK: " + Math.pow(2,  K) + "\t");
+			writer.print(sim.getMisses() + "\t" + sim.getAdresses() + "\t" + (100f * sim.getMisses() / sim.getAdresses()) + "\n");
+
+			K++;
+			
+			if(KN == 64 && K == 5){
+				KN = 256;
+				K = 1;
+			}
+		}
+		
+		K = 1;
+		KN = 64;
+		
+		while(K <= 4){
+
+			Simulation sim = new Simulation(KN, (int) Math.pow(2,  K), Simulation.LRU);
+			sim.run(secondTrace);
+			writer.println(Simulation.LRU + "\tKN: " + KN + "\tK: " + Math.pow(2,  K) + "\t");
+			writer.print(sim.getMisses() + "\t" + sim.getAdresses() + "\t" + (100f * sim.getMisses() / sim.getAdresses()) + "\n");
 
 			K++;
 			
